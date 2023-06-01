aa9ca60fbbc8bf962c2e615686f8aaa8c622d0ed
Strahinja Vujinovic <vujinovicstrahinja123@gmail.com>
2023-06-01 14:13:06 +0200

    styling sign in ui

--- src/Signin.tsx
+++ src/Signin.tsx
@@ -1,21 +1,22 @@
 "use client";
 import React from "react";
 import { signIn } from "next-auth/react";
+
 const Signin = () => {
   return (
     <section className="bg-black opacity-80 h-[100vh] py-20 lg:py-[120px]">
       <div className="container mx-auto ">
         <div className="-mx-4 flex flex-wrap ">
           <div className="w-full px-4 ">
-            <div className="relative mx-auto max-w-[525px] overflow-hidden rounded-lg bg-white py-16 px-10 text-center sm:px-12 md:px-[60px] bg-gradient-to-r to-red-300 from-sky-300">
+            <div className="relative mx-auto max-w-[525px] overflow-hidden rounded-lg bg-white py-16 px-10 text-center sm:px-12 md:px-[60px] bg-gradient-to-r to-black  from-white border">
               <div className="mb-10 text-center md:mb-16">
-                <h1 className="text-5xl font-extrabold text-gray-900">
-                  <span className="text-transparent bg-clip-text bg-gradient-to-r to-red-500 from-sky-700">
+                <h1 className="text-5xl font-extrabold">
+                  <span className="text-transparent bg-clip-text bg-gradient-to-r to-black from-black opacity-90">
                     Scrum Board
                   </span>
                 </h1>
               </div>
-              <p className="mb-6 text-base text-gray-500 font-semibold">
+              <p className="mb-6 text-base text-black font-semibold">
                 Connect With
               </p>
               <ul className="-mx-2 mb-12 flex justify-between">
--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Signin from "@/Signin";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { SessionProvider } from "next-auth/react";
 
 const SignInPage = async () => {
   return <Signin />;
